1098bff311dfa12710b1c6e13cc51e35666ded3f
zkhalapyan <zkhalapyan@gmail.com>
2012-09-06 10:03:14 -0700

    Rewrote client saving logic on the node server.

--- node/ChatServer.js
+++ node/ChatServer.js
@@ -3,48 +3,43 @@
 // Optional. You will see this name in eg. 'ps' or 'top' command
 process.title = 'chatmate-server'
 
-// Port where we'll run the websocket server
 var webSocketsServerPort = 3000;
-
-// List of currently connected clients (users)
-var clients = [ ];  
+var clients = {};  
+var nextClientId = 0;
 
 var WebSocketServer = require('ws').Server;
 var http = require('http');
 
+var log = function( message ) {
+    console.log((new Date()) + " " + message);
+};
 
-/**
- * HTTP server
- */
-var server = http.createServer(function(request, response) {
-    // Not important for us. We're writing WebSocket server, not HTTP server.
-});
+var server = http.createServer();
 server.listen(webSocketsServerPort, function() {
-    console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
+    log("Server is listening on port " + webSocketsServerPort);
 });
 
-// create the server
+
 var wsServer = new WebSocketServer({
     server: server
 });
 
 wsServer.on('connection', function( ws ) {
-    var index = clients.length;
-    console.log((new Date()) + " Accepted connection from client.");
-    clients.push(ws);
+    var clientId = nextClientId++;
+    clients[ clientId ] = ws;
+    log( "Accepted connection from client " + clientId + "." );
     
-    ws.on('message', function( message ) {
-        console.log((new Date()) + "Received messagge: " + message);
-        for(var i = 0; i < clients.length; i++){
-            if( i !== index ) {
-                clients[i].send(message);
+    ws.on( 'message', function( message ) {
+        log( "Received messagge: " + message );
+        for ( var id in clients ) {
+            if( parseInt( id, 10) !== clientId ) {
+                clients[ id ].send(message);
             }
         }
     });
 
     ws.on('close', function() {
-        console.log((new Date()) + " Client disconnected.");
-        clients.splice(index, 1);
-        console.log((new Date()) + " Current client count: " + clients.length );
+        log( "Client " + clientId + " disconnected." );
+        delete clients[ clientId ];
     });
 });

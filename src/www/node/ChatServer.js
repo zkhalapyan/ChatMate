d4a66abd4a4a575d92c382723a7f42325b9d1c9c
Zorayr Khalapyan <zkhalapyan@gmail.com>
2013-02-08 22:04:03 -0800

    Added WS node module locally to run the chat server.

--- src/www/node/ChatServer.js
+++ src/www/node/ChatServer.js
@@ -1,45 +1,52 @@
-"use strict";
 
-// Optional. You will see this name in eg. 'ps' or 'top' command
-process.title = 'chatmate-server'
+(function () {
+    "use strict";
 
-var webSocketsServerPort = 3000;
-var clients = {};  
-var nextClientId = 0;
+    // Optional. You will see this name in eg. 'ps' or 'top' command
+    process.title = 'chatmate-server';
 
-var WebSocketServer = require('ws').Server;
-var http = require('http');
+    var webSocketsServerPort = 3000;
+    var clients = {};
+    var nextClientId = 0;
 
-var log = function( message ) {
-    console.log((new Date()) + " " + message);
-};
+    var WebSocketServer = require('ws').Server;
+    var http = require('http');
 
-var server = http.createServer();
-server.listen(webSocketsServerPort, function() {
-    log("Server is listening on port " + webSocketsServerPort);
-});
+    var log = function (message) {
+        console.log((new Date()) + " " + message);
+    };
+
+    var server = http.createServer();
+    server.listen(webSocketsServerPort, function () {
+        log("Server is listening on port " + webSocketsServerPort);
+    });
 
 
-var wsServer = new WebSocketServer({
-    server: server
-});
+    var wsServer = new WebSocketServer({
+        server: server
+    });
 
-wsServer.on('connection', function( ws ) {
-    var clientId = nextClientId++;
-    clients[ clientId ] = ws;
-    log( "Accepted connection from client " + clientId + "." );
-    
-    ws.on( 'message', function( message ) {
-        log( "Received messagge: " + message );
-        for ( var id in clients ) {
-            if( parseInt( id, 10) !== clientId ) {
-                clients[ id ].send(message);
+    wsServer.on('connection', function (ws) {
+        var clientId = nextClientId += 1;
+        clients[clientId] = ws;
+        log("Accepted connection from client " + clientId + ".");
+        ws.on('message', function (message) {
+            log("Received message: " + message);
+            var id;
+            for (id in clients ) {
+                if (clients.hasOwnProperty(id)) {
+                    if (parseInt(id, 10) !== clientId) {
+                        clients[id].send(message);
+                    }
+                }
+
             }
-        }
+        });
+
+        ws.on('close', function () {
+            log("Client " + clientId + " disconnected.");
+            delete clients[clientId];
+        });
     });
 
-    ws.on('close', function() {
-        log( "Client " + clientId + " disconnected." );
-        delete clients[ clientId ];
-    });
-});
+}());
